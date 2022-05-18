56b0c3fc125f814dcc8288b4d1d00cedbfd7923c
Lisandro Alejo <amlisandro18@gmail.com>
2022-05-18 20:14:29 -0300

    avances realizados

--- Tp03-AlejoMarioLisandro/src/main/java/ar/edu/unju/edm/controller/UsuarioController.java
+++ Tp03-AlejoMarioLisandro/src/main/java/ar/edu/unju/edm/controller/UsuarioController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.ModelAndView;
 
+
+import org.springframework.ui.Model;
+
 import ar.edu.unju.edm.model.Usuario;
 import ar.edu.unju.edm.util.ListadoUsuarios;
 
@@ -15,18 +18,20 @@ public class UsuarioController {
 	Usuario nuevoUsuario;
 	@Autowired
 	ListadoUsuarios lista;
-	@GetMapping("/nuevoUsuario")
+	@GetMapping("/cargarusuario")
 	public ModelAndView addUser() {
 		ModelAndView vista = new ModelAndView("cargarusuario");
 		vista.addObject("usuario", "nuevoUsuario");
 		return vista;
 		
 	}
-	@GetMapping("/guardarUsuario")
-	public String saveUser(@ModelAttribute ("usuario") Usuario usuarioparaguardar) {
+	
+	@GetMapping("/mostrarusuario")
+	public String saveUser(@ModelAttribute ("usuario") Usuario usuarioparaguardar, Model model) {
 		lista.getListado().add(usuarioparaguardar);
 		System.out.print("Tamaño listado: " + lista.getListado().size());
-		return "redirect:/otroUsuario";
+		model.addAttribute("listadoUsuarios", lista.getListado());
+		return "mostrarusuario";
 	}
-	
+
 }
