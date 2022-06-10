6dcfbaf49f9a7ab58ce86872a717ca2437e30fa7
Lisandro Alejo <amlisandro18@gmail.com>
2022-06-10 16:56:45 -0300

    Errores

--- Tp03-AlejoMarioLisandro/src/main/java/ar/edu/unju/edm/model/Curso.java
+++ Tp03-AlejoMarioLisandro/src/main/java/ar/edu/unju/edm/model/Curso.java
@@ -3,12 +3,11 @@ package ar.edu.unju.edm.model;
 import java.time.LocalDate;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
-
-import org.hibernate.validator.constraints.NotEmpty;
+import javax.validation.constraints.NotEmpty;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Component;
 
@@ -21,75 +20,79 @@ public class Curso {
    private Integer duracion;
    private String descripcion;
    private Integer cupo;
-   private double costo;
+   private Double costo;
    private Integer valoracion;
-   ///
-	private String mail;
+
 	private String contrasena;
-	@NotEmpty
-	private String nombre;
-	@Max (value = 99999999, message="DNI debe ser menor que 99999999")
-	@Min(value = 1000000, message="DNI debe ser mayor que 1000000")
+	@NotNull
 	@Id
-<<<<<<< Updated upstream
-	private Integer dni;
-=======
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
->>>>>>> Stashed changes
 	private Boolean estado;
-	@DateTimeFormat(pattern = "yyyy-MM-dd") //dd/MM/yyyy
-	  private LocalDate fechaInicio;
-	  private LocalDate fechaFinal;
 	public Curso() {
 		// TODO Auto-generated constructor stub
 	}
-	private int Id;
 	@NotEmpty
 	//Guardad canedas
 	private String nombre;
-	@NotEmpty
-	private String descripcion;
-	private String valoracion;
-	private String Docente;
-	//Guarda numeros
-	private float costo;
-	private double duracion;
-	private int cupo;
-	@NotEmpty
-	private String nombreUsuario;
-	private String email;
-	@NotEmpty //Para String
-	private String contrasena;
-	@Min(value=1000000, message="El DNI debe ser mayor que un millon")
-	@Max(value=99999999, message="El DNI debe ser menor que un 100 millones")
-	@NotNull //Para numeros
-	private Long dni;
+
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private LocalDate fechadeI;
-	private Boolean estado;
-	public Usuario(int id, String nombre, String apellido, String nombreUsuario, String contrasena, String email, Long dni, LocalDate fechadeN) {
-		super();
-		Id = id;
-		this.nombre = nombre;
-		this.apellido = apellido;
-		this.nombreUsuario = nombreUsuario;
-		this.contrasena = contrasena;
-		this.email = email;
-		this.dni = dni;
-		this.fechadeN = fechadeN;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private LocalDate fechadeF;
+	public String getDocente() {
+		return docente;
 	}
-	public LocalDate getFechadeN() {
-		return fechadeN;
+	public void setDocente(String docente) {
+		this.docente = docente;
 	}
-	public void setFechadeN(LocalDate fechadeN) {
-		this.fechadeN = fechadeN;
+	public Integer getDuracion() {
+		return duracion;
 	}
-	public int getId() {
-		return Id;
+	public void setDuracion(Integer duracion) {
+		this.duracion = duracion;
 	}
-	public void setId(int id) {
-		Id = id;
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public Integer getCupo() {
+		return cupo;
+	}
+	public void setCupo(Integer cupo) {
+		this.cupo = cupo;
+	}
+	public double getCosto() {
+		return costo;
+	}
+	public void setCosto(double costo) {
+		this.costo = costo;
+	}
+	public Integer getValoracion() {
+		return valoracion;
+	}
+	public void setValoracion(Integer valoracion) {
+		this.valoracion = valoracion;
+	}
+	public LocalDate getFechadeI() {
+		return fechadeI;
+	}
+	public void setFechadeI(LocalDate fechadeI) {
+		this.fechadeI = fechadeI;
+	}
+	public LocalDate getFechadeF() {
+		return fechadeF;
+	}
+	public void setFechadeF(LocalDate fechadeF) {
+		this.fechadeF = fechadeF;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
 	}
 	public String getNombre() {
 		return nombre;
@@ -103,44 +106,12 @@ public class Curso {
 	public void setContrasena(String contrasena) {
 		this.contrasena = contrasena;
 	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-<<<<<<< Updated upstream
-	public String getApellido() {
-		return apellido;
-	}
-	public void setApellido(String apellido) {
-		this.apellido = apellido;
-	}
-	public String getNombreUsuario() {
-		return nombreUsuario;
-	}
-	public void setNombreUsuario(String nombreUsuario) {
-		this.nombreUsuario = nombreUsuario;
-=======
-	public Integer getId() {
-		return id;
-	}
 
-	public void setId(Integer id) {
-		this.id = id;
->>>>>>> Stashed changes
-	}
 	public Boolean getEstado() {
 		return estado;
 	}
 	public void setEstado(Boolean estado) {
 		this.estado = estado;
-	}
-	public Long getDni() {
-		return dni;
-	}
-	public void setDni(Long dni) {
-		this.dni = dni;
 	}
 	
 }
